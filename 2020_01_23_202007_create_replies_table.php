650ec74c4aa3f8712677402678490674b10d4b59
WacuMbugua <58767172+WacuMbugua@users.noreply.github.com>
2020-01-25 03:07:10 +0300

    Day Two

--- 2020_01_23_202007_create_replies_table.php
+++ 2020_01_23_202007_create_replies_table.php
@@ -15,8 +15,8 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id'); 
-            $table->string('title');
+            $table->integer('thread_id'); 
+            $table->integer('user_id');
             $table->text('body');
             $table->timestamps();
         });
